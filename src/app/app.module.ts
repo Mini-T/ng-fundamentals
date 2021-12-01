1ee9f74fd67235aaaf014ab221d2cb103a6e4c7e
Mini-T <Minilucker.74@gmail.com>
2021-12-01 21:56:20 +0100

    from parent to child input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/events-thumbnail.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
   ],
   imports: [
     BrowserModule
